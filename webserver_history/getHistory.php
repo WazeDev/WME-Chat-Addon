f4ca9e5982c99743d8c059a417e76a514bf7b889
justins83 <justins83.waze@gmail.com>
2018-06-28 16:41:03 -0400

    Update getHistory.php
    php 7 changes

--- webserver_history/getHistory.php
+++ webserver_history/getHistory.php
@@ -11,13 +11,13 @@ $room = $_GET['room'];
 /*if ($room!='France')
     die ('[{"username":"ChatAddon","message":"Chat history disabled because of server overload.","datetime":""}]');*/
 
-$link = mysql_connect("localhost", "clog", "hT5_sX23");
+$link = mysqli_connect("localhost", "clog", "hT5_sX23");
 /*var_dump ($link);*/
 
-$db_selected = mysql_select_db('clog', $link);
+$db_selected = mysqli_select_db($link, 'clog');
 
 $query="SET NAMES 'utf8'";
-$result = mysql_query($link, $query);
+$result = mysqli_query($link, $query);
 
 $transRoomNameFromDB2HR=array('_' => ' ');
 $transRoomNameFromHR2DB=array(' ' => '_');
@@ -26,16 +26,16 @@ $table = strtr($room, $transRoomNameFromHR2DB) . "_room";
 
 $query="SELECT * FROM " . $table . " ORDER BY datetime DESC, id DESC LIMIT 0,10";
 /*var_dump($query);*/
-$result = mysql_query($link, $query);
+$result = mysqli_query($link, $query);
 if (!$result) /* no history for this room (i.e. no table)? or any other mysql error*/
 {
-	mysql_close($link);
+	mysqli_close($link);
 	die ("[]");
 }
 
 $data = array();
 
-while ($row = mysql_fetch_assoc($result)) {
+while ($row = mysqli_fetch_assoc($result)) {
 		$msg = array();
 		$msg["username"]=$row['username'];
 		$msg["message"]=htmlspecialchars(str_replace('\\', '', strip_tags($row['message'])));
@@ -52,5 +52,5 @@ $msg["datetime"]=date("2017-07-03 15:40:00");
 array_push($data, $msg);*/
 
 echo json_encode(array_reverse($data));
-mysql_close($link);
+mysqli_close($link);
 ?>
